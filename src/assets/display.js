c1cf215b5594f641c74a90e95e1bc57a1523983f
kenan238 <kenanyazbeck@gmail.com>
2021-11-27 21:37:13 +0200

    Adding logos

--- src/assets/display.js
+++ src/assets/display.js
@@ -24,5 +24,6 @@ const printInfoAllAtOnce = (data) => {
 
 module.exports = {
     printInfo,
-    printInfoAllAtOnce
+    printInfoAllAtOnce,
+    term
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,59 @@
 const { getInformation } = require("./assets/fetch.js");
-const { printInfoAllAtOnce } = require("./assets/display.js")
+const { printInfoAllAtOnce, term } = require("./assets/display.js")
 
 var info = getInformation();
+var logos = {
+    "windowsnew": `################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################
+################  ################`,
+    "windows10": `                   .oodMMMMMMMMMMMMM
+       ..oodMMM  MMMMMMMMMMMMMMMMMMM
+ oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ 
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+ MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM
+    \`^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM
+          \`\`\`\`^^^^  ^^MMMMMMMMMMMMMMMMM
+                         \`\`\`\`^^^^^^MMMM`,
+    "windowsold": `
+       :tt:::tt333EE3  .et=:!!t3Z3z.,
+       Et:::ztt33EEEL$ @Ee.,      ..,
+      ;tt:::tt333EE7$ ;EEEEEEttttt33#
+     :Et:::zt333EEQ.$ $EEEEEttttt33QL
+     it::::tt333EEF$ @EEEEEEttttt33F
+    ;3=*^\`\`\`"*4EEV$ :EEEEEEttttt33@.
+    ,.=::::!t=., $\`$ @EEEEEEtttz33QF
+   ;::::::::zt33)$   "4EEEtttji3P*
+  :t::::::::tt33.$:Z3z..$  \`\`$ ,..g.
+  i::::::::zt33F$ AEEEtttt::::ztF
+ ;:::::::::t33V$ ;EEEttttt::::t3
+ E::::::::zt33L$ @EEEtttt::::z3F
+{3=*^\`\`\`"*4E3)$ ;EEEtttt:::::tZf
+             \`$ :EEEEtttt::::z7
+                 "VEzjt:;;z>*`
+}
 
 // Display the system information in neofetch style
+term.green(logos["windowsold"])
 printInfoAllAtOnce({
     [info.hostname]: true,
     "OS": `Windows Version ${info.winver}`,
